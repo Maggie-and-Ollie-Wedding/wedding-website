7787c5cc74f4d4eadc6c7fa3a12fc1c49227d7d9
Maggie Hunt <maggie.hunt@paconsulting.com>
2022-12-19 09:36:26 +0000

    python file

---
+++ main.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template, request, Response #imports functions to use python with html
+import os
+
+app = Flask("magie-and-ollie-wedding") #making an app
+
+#Homepage
+@app.route("/")  
+def landing_page():
+        return render_template("index.html")
+
+
+###debugging
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
+###app.run(debug=True) #runs the app. the debug part - unlocks debugging feature.
+##app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
