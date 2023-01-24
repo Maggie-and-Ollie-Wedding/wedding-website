fa1c8c10943aa5b57dded27cca42a4d5d920ca08
Maggie Hunt <maggie.hunt@paconsulting.com>
2023-01-24 12:31:30 +0000

    order of events

--- static/js/script.js
+++ static/js/script.js
@@ -10,4 +10,17 @@ window.addEventListener('load', (event) => {
     )
           }})
 
-  
+
+
+ const rsvpDietSwitch1 = document.getElementById("RSVPDiet1")
+ const rsvpDietDropdown1 = document.getElementById("dropdownDiet1")
+ const rsvpVocalPartSwitch1 = document.getElementById("RSVPChoir1")
+ const rsvpVocalPartDropdown1 = document.getElementById("dropdownVocalPart1")
+   
+ function rsvpDiet1() {
+ if (rsvpDietSwitch1 === true) {
+  rsvpDietDropdown1.style.display = "block";
+} else {
+  rsvpDietDropdown1.style.display = "none";
+}
+ }
