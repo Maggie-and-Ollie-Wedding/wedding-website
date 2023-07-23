12f316240d6cf09475b5c156e5b43c03f044aa11
Maggie Hunt <maggie.hunt@paconsulting.com>
2023-07-23 01:23:06 +0100

    photos, new index page layout

--- static/js/script.js
+++ static/js/script.js
@@ -33,42 +33,38 @@ function myFunction () {
   }
 }
 
-
-
 function toggleFormSection (row) {
   var RSVP = document.getElementById('RSVPCheck' + row)
   var RSVPDetails = document.getElementById('RSVPDetails' + row)
   var choirCheckbox = document.getElementById('RSVPChoir' + row)
   var choirSection = document.getElementById('dropdownVocalPart' + row)
-  var selectedChoirOption = choirSection.options[choirSection.selectedIndex].value;
+  var selectedChoirOption =
+    choirSection.options[choirSection.selectedIndex].value
   var dietCheckbox = document.getElementById('RSVPDiet' + row)
   var dietSection = document.getElementById('dropdownDiet' + row)
-  var selectedDietOption = dietSection.options[dietSection.selectedIndex].value;
-  var dietDetailColumn = document.getElementById("diet-detail-column"+row)
-  var RSVPNo = document.getElementById("RSVPNo"+row)
-  var choirYes = document.getElementById("ChoirYes"+row)
-  var dietary = document.getElementById("dietary"+row)
-  var DietNo = document.getElementById("DietNo"+row)
-  var DietYes = document.getElementById("DietYes"+row)
-  var voicePartGuest = document.getElementById("voicePartGuest"+row)
-  var dietOption = document.getElementById("dietOption"+row)
-  var dietDetail = document.getElementById('dietDetail'+row)
-  var dietDetailText = document.getElementById('dietDetailText'+row)
-
-
+  var selectedDietOption = dietSection.options[dietSection.selectedIndex].value
+  var dietDetailColumn = document.getElementById('diet-detail-column' + row)
+  var RSVPNo = document.getElementById('RSVPNo' + row)
+  var choirYes = document.getElementById('ChoirYes' + row)
+  var dietary = document.getElementById('dietary' + row)
+  var DietNo = document.getElementById('DietNo' + row)
+  var DietYes = document.getElementById('DietYes' + row)
+  var voicePartGuest = document.getElementById('voicePartGuest' + row)
+  var dietOption = document.getElementById('dietOption' + row)
+  var dietDetail = document.getElementById('dietDetail' + row)
+  var dietDetailText = document.getElementById('dietDetailText' + row)
 
   if (RSVP.checked) {
     RSVPDetails.style.display = 'flex' // show the section
     RSVPNo.style.display = 'none'
     dietary.style.display = 'inline'
-    RSVPResponse = "Yes"
-    
+    RSVPResponse = 'Yes'
   } else {
     RSVPNo.style.display = 'inline'
     RSVPDetails.style.display = 'none' // hide the section
     choirYes.style.display = 'none'
     dietary.style.display = 'none'
-    RSVPResponse = "No"
+    RSVPResponse = 'No'
   }
 
   if (choirCheckbox.checked) {
@@ -76,74 +72,115 @@ function toggleFormSection (row) {
     choirYes.style.display = 'inline'
     voicePartGuest.innerHTML = selectedChoirOption
     VocalPartRespons = selectedChoirOption
-    ChoirResponse = "Yes"
+    ChoirResponse = 'Yes'
   } else {
     choirSection.style.display = 'none' // hide the section
     choirSection.value = 'Vocal part'
     choirYes.style.display = 'none'
-    ChoirResponse = "No"
+    ChoirResponse = 'No'
   }
 
   if (dietCheckbox.checked) {
     dietSection.style.display = 'block' // show the section
     DietYes.style.display = 'inline'
     DietNo.style.display = 'none'
-    DietResponse = "Yes:"
-    
+    DietResponse = 'Yes:'
 
     if (selectedDietOption === 'Multiple/Other') {
-      console.log(" multi")
+      console.log(' multi')
       dietDetail.style.display = 'block' // show the section
-      dietDetailColumn.style.width= "40vw"
-      dietOption.style.display='none'
+      dietDetailColumn.style.width = '40vw'
+      dietOption.style.display = 'none'
       dietOption.value = selectedDietOption
-      dietDetail.addEventListener('input', function() {
-      dietDetailText.innerHTML = dietDetail.value
-      DietResponseDetail = dietDetail.value
-      console.log(" multi text")
-        });
-        
-      } else {
-        dietDetail.style.display = 'none' // hide the section
-        dietDetailColumn.style.width= "0%"
-        dietOption.innerHTML= selectedDietOption
-        // dietSection.value = 'Dietary Requirements'
-        DietResponseDetail = selectedDietOption
-        dietDetailText.innerHTML = ""
-        console.log(selectedDietOption)
-        dietOption.style.display="inline"
-        dietOption.innerHTML= selectedDietOption
-        console.log("not multi")
-      }
+      dietDetail.addEventListener('input', function () {
+        dietDetailText.innerHTML = dietDetail.value
+        DietResponseDetail = dietDetail.value
+        console.log(' multi text')
+      })
     } else {
-      dietSection.style.display = 'none' // hide the section
       dietDetail.style.display = 'none' // hide the section
-      dietDetailColumn.style.width= "0%"
-      dietary.style.display="none"
-      dietDetail.value = ""
-      dietDetailText.innerHTML=""
-      dietary.style.width="0%"
-      dietSection.value = 'Dietary Requirements'
-      DietYes.style.display = 'none'
-      DietNo.style.display = 'inline'
-      DietResponse = "No"
-      console.log("no")
+      dietDetailColumn.style.width = '0%'
+      dietOption.innerHTML = selectedDietOption
+      // dietSection.value = 'Dietary Requirements'
+      DietResponseDetail = selectedDietOption
+      dietDetailText.innerHTML = ''
+      console.log(selectedDietOption)
+      dietOption.style.display = 'inline'
+      dietOption.innerHTML = selectedDietOption
+      console.log('not multi')
     }
-
-  
+  } else {
+    dietSection.style.display = 'none' // hide the section
+    dietDetail.style.display = 'none' // hide the section
+    dietDetailColumn.style.width = '0%'
+    dietary.style.display = 'none'
+    dietDetail.value = ''
+    dietDetailText.innerHTML = ''
+    dietary.style.width = '0%'
+    dietSection.value = 'Dietary Requirements'
+    DietYes.style.display = 'none'
+    DietNo.style.display = 'inline'
+    DietResponse = 'No'
+    console.log('no')
+  }
 }
 
+function reveal (section) {
+  var sections = {
+    gettingThere: document.getElementById('getting-there'),
+    accommodation: document.getElementById('accommodation'),
+    dressCode: document.getElementById('dress-code'),
+    giftRegistry: document.getElementById('gift-registry'),
+    choir: document.getElementById('choir'),
+    gettingThereCheck: document.getElementById('getting-there-check'),
+    accommodationCheck: document.getElementById('accommodation-check'),
+    dressCodeCheck: document.getElementById('dress-code-check'),
+    giftRegistryCheck: document.getElementById('gift-registry-check'),
+    choirCheck: document.getElementById('choir-check')
+  }
 
+  var checkbox = section + 'Check'
+  var isChecked = sections[checkbox].checked
 
-// function submitForm() {
-//   for person in people:
-//   console.log(
-  //   "Name : ", person,
-  //   ", RSVP : ", RSVPResponse,
-  //   ", Choir : ", ChoirResponse,
-  //   ", Vocal_Part : ", VocalPartResponse,
-  //   ", Dietary_Requirements : ", DietResponse,
-  //   ", Dietary_Requirements_Detail : ", DietResponseDetail
-  // )
-// }
+  if (isChecked) {
+    sections[section].style.display = 'none'
+    sections[checkbox].checked = false
+  } else {
+    if (section in sections) {
+      sections[section].style.display = 'block'
+      sections[checkbox].checked = true
 
+      if (section == 'gettingThere') {
+        sections.accommodation.style.display = 'none'
+        sections.accommodationCheck.checked = false
+      }
+
+      if (section == 'accommodation') {
+        sections.gettingThere.style.display = 'none'
+        sections.gettingThereCheck.checked = false
+      }
+
+      if (section == 'dressCode') {
+        sections.giftRegistry.style.display = 'none'
+        sections.choir.style.display = 'none'
+        sections.giftRegistryCheck.checked = false
+        sections.choirCheck.checked = false
+      }
+
+      if (section == 'giftRegistry') {
+        sections.dressCode.style.display = 'none'
+        sections.dressCodeCheck.checked = false
+        sections.choir.style.display = 'none'
+        sections.choirCheck.checked = false
+      }
+
+      if (section == 'choir') {
+        sections.dressCode.style.display = 'none'
+        sections.dressCodeCheck.checked = false
+        sections.giftRegistry.style.display = 'none'
+        sections.giftRegistryCheck.checked = false
+        
+      }
+    }
+  }
+}
