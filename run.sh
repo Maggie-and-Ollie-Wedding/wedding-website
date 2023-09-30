7e68a374760740672af3718cac74397d48071736
Maggie Hunt <maggie.hunt@paconsulting.com>
2023-09-30 19:17:48 +0100

    {stylesheet layout

--- run.sh
+++ run.sh
@@ -4,5 +4,4 @@ export TREEAPP_API_URL="api-test.thetreeapp.org"
 export TREEAPP_API_KEY="b2b_test_sgS2KopWbNrAv2gOfsCVDpcIy8Um9Hxn8CuQHVcCuWHcFCjWwf5gfOVeO9NZ7zx5va5WdLckBkSYwZhsi2NuG9VQaEY0HNAv0E3"
 export EMAIL_API_KEY="re_MEzUCBUK_LToyum42Q6dDx4PKVgCBwGEG"
 export HEROKU_EMAIL_API_KEY="re_eLX3W2fY_3EGzf4P1E9h9EdvjRz8PU7ih"
-
-python3 app.py
+python3 test.py
