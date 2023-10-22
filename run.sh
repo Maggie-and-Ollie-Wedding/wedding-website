a6d1fe7db48d032f24246c91aea528dece4070df
Maggie Hunt <maggie.hunt@paconsulting.com>
2023-10-22 12:56:47 +0100

    break for trees

--- run.sh
+++ run.sh
@@ -3,4 +3,4 @@ export GOOGLE_APPLICATION_CREDENTIALS="maggie-and-ollie-wedding-abcc08241ee5.jso
 export TREEAPP_API_URL="api-test.thetreeapp.org"
 export TREEAPP_API_KEY="b2b_test_sgS2KopWbNrAv2gOfsCVDpcIy8Um9Hxn8CuQHVcCuWHcFCjWwf5gfOVeO9NZ7zx5va5WdLckBkSYwZhsi2NuG9VQaEY0HNAv0E3"
 export EMAIL_API_KEY="re_fnGQipwL_FmXaWvjPETUQQHXgKYFePUmH"
-python3 app.py
+python3 INVITE.py
