35607970ed0a4f2e147b75813e29b0f6a3add8e8
Maggie Hunt <maggie.hunt@paconsulting.com>
2023-11-03 22:44:57 +0000

    added touch to samsung

--- static/js/RSVPlookupscript.js
+++ static/js/RSVPlookupscript.js
@@ -107,7 +107,7 @@ function inviteSearch (invitations) {
     if (androidBool) {
       console.log("android true")
       selectElement.style.display = 'none'
-      selectElement.focus()
+      selectElement.touch()
       
     }
     else {
